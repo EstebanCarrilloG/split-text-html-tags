e539f75c20792576cf48b96d5c7468df31d532f3
Esteban Carrillo <estexav0996@gmail.com>
2023-08-28 15:37:20 -0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,35 @@
+// Espera a que el contenido del DOM se haya cargado completamente
+document.addEventListener("DOMContentLoaded", function () {
+    // Selecciona el elemento que contiene el título de la sección
+    const textContainer = document.querySelector("#sectionTitle");
+    
+    // Obtiene el contenido del título
+    const textToSeparate = sectionTitle.innerHTML;
+  
+    // Divide el título en palabras utilizando un espacio como separador
+    textContainer.innerHTML = textToSeparate.split(" ")
+      .map((e) => {
+        // Crea un contenedor div para cada palabra
+        const wordContainer = document.createElement("div");
+        wordContainer.classList.add("word-container");
+  
+        // Divide la palabra en caracteres individuales
+        const word = e.split("").map((e) => {
+          // Crea un elemento span para cada carácter
+          const character = document.createElement("span");
+          character.classList.add("character");
+          character.innerHTML = e;
+  
+          // Devuelve el código HTML del carácter
+          return character.outerHTML.toString();
+        });
+  
+        // Llena el contenedor de palabra con los caracteres
+        wordContainer.innerHTML = word.join("");
+  
+        // Devuelve el código HTML del contenedor de palabra
+        return wordContainer.outerHTML.toString();
+      })
+      // Combina todas las palabras fragmentadas para reconstruir el título
+      .join(" ");
+  });
